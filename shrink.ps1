f749a1265adcba3a93b38d8dd86bcd4d5b080228
Tsuyoshi OCHIAI <64637968+occhi244@users.noreply.github.com>
2022-10-09 17:43:20 +0900

    Update shrink.ps1
    Correct wrong comment descriptor...

--- shrink.ps1
+++ shrink.ps1
@@ -4,10 +4,10 @@ Add-Type -AssemblyName System.Windows.Forms
 $cbImage = [System.Windows.Forms.Clipboard]::GetImage()
 If ($cbImage -ne $null)
 {
-  $ratio = 71   // Shriink/Expand ratio: 71 means the original image is shrunk to 71%
+  $ratio = 71   # Shriink/Expand ratio: This setting shrink the original image to 71%.
   $ratio = $ratio / 100
-  $newWidth = [int]($cbImage.Width * $ratio)
-  $newHeight = [int]($cbImage.Height * $ratio)
+  $newWidth = [int]($cbImage.Width * $ratio)    # Change this setting
+  $newHeight = [int]($cbImage.Height * $ratio)  # if you change the aspect ratio
   $bmImage = New-Object System.Drawing.Bitmap($newWidth, $newHeight)
   $newImage = [System.Drawing.Graphics]::FromImage($bmImage)
   $newImage.DrawImage($cbImage, $(New-Object -TypeName System.Drawing.Rectangle -ArgumentList 0, 0, $newWidth, $newHeight))
